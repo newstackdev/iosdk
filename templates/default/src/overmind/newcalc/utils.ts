b810a1db4151d8aded48c1f972fd0fe228e01b07
Igor Rubinovich <igor.rubinovich@gmail.com>
2022-04-11 19:14:19 +0200

    remove extraneous

--- templates/default/src/overmind/newcalc/utils.ts
+++
@@ -1,3 +0,0 @@
-export const isNum = (n: any) => Number(n) == n;
-export function lastNum<T extends (string | number)> (arr: T[]) { return arr.filter(n => isNum(n)).pop() as number; }
-export const last = <T>(arr: T[]) => arr[arr.length - 1];
