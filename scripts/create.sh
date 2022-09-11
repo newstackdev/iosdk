bf9345883c9b0d224a3ddba83938dd7b2937b6a0
Igor Rubinovich <igor.rubinovich@gmail.com>
2022-09-11 17:08:47 +0200

    cd fix for macos create.sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,8 +2,9 @@ echo Installing IOSDK, this will take a while...
 
 # npx create-react-app $1 --template typescript
 # canonicalFile=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)/$(basename -- "$0")")
-canonicalFile=$(cd -P -- "$(dirname -- "$0")" && printf "%s\n" "$(pwd -P)/$(basename -- "$0")")
-canonicalDir=$(dirname "$canonicalFile")
+# canonicalFile=$(cd -P -- "$(dirname -- "$0")" && printf "%s\n" "$(pwd -P)/$(basename -- "$0")")
+# canonicalDir=$(dirname "$canonicalFile")
+canonicalDir=`pwd`
 target=$canonicalDir/$1
 mkdir $canonical/$1
 cd $target
